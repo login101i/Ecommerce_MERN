cc21814d617a328d7a3ae04e7b1997056d83b582
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2020-12-31 20:16:24 +0100

    User Reviews

--- routes/product.js
+++ routes/product.js
@@ -7,7 +7,10 @@ const {
     newProduct,
     getSingleProduct,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    createProductReview,
+    getProductReviews,
+    deleteReview
 } = require('../controllers/productController')
 
 
@@ -22,6 +25,11 @@ router.route('/admin/product/new').post(isAuthenticatedUser, newProduct)
 router.route('/admin/product/:id')
     .put(isAuthenticatedUser, authorizeRoles('admin'),  updateProduct)
     .delete(isAuthenticatedUser, authorizeRoles('admin'),  deleteProduct)
+
+
+router.route('/review').put(isAuthenticatedUser, createProductReview)
+router.route('/reviews').get(isAuthenticatedUser, getProductReviews)
+router.route('/reviews').delete(isAuthenticatedUser, deleteReview)
 
 
 module.exports = router
